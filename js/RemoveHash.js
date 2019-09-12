b3119b1e7f4d645cc50b525df903a9432aef942e
Tyler Chapman <tyler.chapman>
2019-09-12 00:35:01 -0400

    Few tweaks to the has removal js script.

--- js/RemoveHash.js
+++ js/RemoveHash.js
@@ -1,6 +1,11 @@
 /* Used for removal of the has after a redirect */
 function locationHashChanged() {
 	var deleteIndex = window.location.href.indexOf("#");	
-	 history.replaceState({}, '', window.location.href.slice(0, deleteIndex));
+	 if(deleteIndex != -1)
+	 {
+	 	history.replaceState({}, '', window.location.href.slice(0, deleteIndex));
+	 }
 }
 window.onhashchange = locationHashChanged;
+
+window.onload = locationHasChanged;
